48d622bde91f37d9a2b5eda4f7ea88875b81f157
Lekkyjay <lekkyjayjay@yahoo.com>
2021-09-19 17:49:31 +0200

    Added navigation

---
+++ client/src/components/Nav.tsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <nav>
+      <div className="container nav">
+        <img src="./images/lime.svg" alt="Lime logo" className="logo" />
+        <h1 className="brand">LimeCRM</h1>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
